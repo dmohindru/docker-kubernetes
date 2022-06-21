94cefb9192abdd998a92c05b333a0662686b4480
Dhruv Mohindru <dhruv.mohindru@parcelpoint.com.au>
2022-06-21 17:59:58 +1000

    Finished section 5

--- docker-kubernets-course/section5-docker-compose-with-multiple-local-containers/visits/index.js
+++ docker-kubernets-course/section5-docker-compose-with-multiple-local-containers/visits/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
 // Important: here host redis-server will be replaced by docker compose with actual
@@ -11,6 +12,9 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
+  // make sure our server always shutdown when ever to get a request on / route
+  // This piece of code is to demonstrate docker-compose restart policy
+  process.exit(0);
   client.get('visits', (err, visits) => {
     res.send('Number of visits is ' + visits);
     client.set('visits', parseInt(visits) + 1);
